2191931dd362172fabe92828d1429f478e2e1b53
turtle601 <turtle601@naver.com>
2024-01-13 14:42:20 +0900

    feat: Modal 기능 중 CloseButton 컴포넌트 구현

---
+++ src/components/Modal/components/CloseButton/CloseButton.tsx
@@ -0,0 +1,21 @@
+import { ComponentPropsWithoutRef } from 'react';
+import { getCustomChildren } from '../../../../utils/getCustomChildren';
+import { useModalStore } from '../../hooks/modal';
+
+interface CloseButtonProps extends ComponentPropsWithoutRef<'button'> {
+  isCustom: boolean;
+}
+
+function CloseButton({ isCustom, children, ...rest }: CloseButtonProps) {
+  const { closeModal } = useModalStore((state) => ({
+    closeModal: state.closeModal,
+  }));
+
+  return isCustom ? (
+    getCustomChildren(children, { onClick: { closeModal }, ...rest })
+  ) : (
+    <button className="close-button">{children}</button>
+  );
+}
+
+export default CloseButton;
