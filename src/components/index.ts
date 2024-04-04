52ff33eb7376735dd22968d23f2206ddbeac2176
turtle601 <turtle601@naver.com>
2024-04-04 14:33:56 +0900

    fix: 배포 오류로 인한 수정

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,4 @@ export { default as Modal } from './Modal';
 export { default as Flex } from './layout/Flex';
 export { default as Center } from './layout/Center';
 export { default as Grid } from './layout/Grid';
+export { default as StackAnimation } from './StackAnimation';
